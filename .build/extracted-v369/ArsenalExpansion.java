2d76ed035f966ecbacab0d3bdf7ed4712cf50ffa
BetterCallRais <raisyamuhammad337@gmail.com>
2026-09-20 14:20:21 +0700

    Add ray trace import for Zephya Windburst leap

--- .build/extracted-v369/ArsenalExpansion.java
+++ .build/extracted-v369/ArsenalExpansion.java
@@ -22,6 +22,7 @@ import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
+import org.bukkit.util.RayTraceResult;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
